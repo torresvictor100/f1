ffe769ed9acabdc6cc7e90ac6b37dbf553642926
torresvictor100 <torresvictor100@gmail.com>
2023-11-25 11:00:11 -0300

    ta fucionando agr é melhorar

---
+++ f1-graphics/src/views/admin/home/components/DriverStandingsStatic.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { Flex, FormLabel, Avatar, Select, Box } from '@chakra-ui/react';
+import MiniStatisticsTitle from 'components/card/MiniStatisticsTitle';
+import PlaceholderImage from 'assets/img/comparationOfDrivers/Mclaren.png';
+
+interface DriverStanding {
+  position: string;
+  positionText: string;
+  points: string;
+  wins: string;
+  Driver: {
+    driverId: string;
+    url: string;
+    code: string;
+    permanentNumber: string;
+    givenName: string;
+    familyName: string;
+    dateOfBirth: string;
+    nationality: string;
+  };
+  Constructors: Array<{
+    constructorId: string;
+    url: string;
+    name: string;
+    nationality: string;
+  }>;
+}
+
+interface DriverSelectProps {
+  driverStandings: DriverStanding[];
+}
+
+const DriverStandingsStatic: React.FC<DriverSelectProps> = ({ driverStandings }) => {
+  const [selectedDriver, setSelectedDriver] = useState<DriverStanding>(driverStandings[0]);
+
+  const handleDriverChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedDriverId = event.target.value;
+    const selectedDriver = driverStandings.find(driver => driver.Driver.driverId === selectedDriverId);
+    setSelectedDriver(selectedDriver || driverStandings[0]);
+  };
+
+  return (
+    <Box width="calc(100% - -100px)">
+      <MiniStatisticsTitle
+        endContent={
+          <Flex me="-16px" mt="10px">
+            <FormLabel htmlFor="balance">
+              <Avatar src={selectedDriver.Driver.url || PlaceholderImage} />
+            </FormLabel>
+            <Select
+              id="balance"
+              mt="5px"
+              me="0px"
+              defaultValue={selectedDriver.Driver.driverId}
+              onChange={handleDriverChange}
+            >
+              {driverStandings.map(driver => (
+                <option key={driver.Driver.driverId} value={driver.Driver.driverId}>
+                  {driver.Driver.givenName} {driver.Driver.familyName}
+                </option>
+              ))}
+            </Select>
+          </Flex>
+        }
+        title="Driver Points"
+        name={`${selectedDriver.Driver.givenName} ${selectedDriver.Driver.familyName}`}
+        value={selectedDriver.points}
+      />
+    </Box>
+  );
+};
+
+export default DriverStandingsStatic;
--- f1-graphics/src/views/admin/home/components/StatisticsTable.tsx
+++ f1-graphics/src/views/admin/home/components/StatisticsTable.tsx
@@ -6,123 +6,117 @@ import MiniStatisticsGrafic from 'components/card/MiniStatisticsTitle'
 import MAX from 'assets/img/comparationOfDrivers/max-verstappen.png';
 import RBR from 'assets/img/comparationOfDrivers/Red-Bull-logo.jpg';
 import ConstructorStandingsStatic from './ConstructorStandingsStatic';
+import DriverStandingsStatic from './DriverStandingsStatic';  // Importa o novo componente
 
 type DriverStanding = {
-	position: string;
-	positionText: string;
-	points: string;
-	wins: string;
-	Driver: {
-	  url: string;
-	  code: string;
-	  permanentNumber: string;
-	  givenName: string;
-	  familyName: string;
-	  dateOfBirth: string;
-	  nationality: string;
-	  driverId: string;
-	};
-	Constructors: Array<{
-	  constructorId: string;
-	  url: string;
-	  name: string;
-	  nationality: string;
-	}>;
+  position: string;
+  positionText: string;
+  points: string;
+  wins: string;
+  Driver: {
+    url: string;
+    code: string;
+    permanentNumber: string;
+    givenName: string;
+    familyName: string;
+    dateOfBirth: string;
+    nationality: string;
+    driverId: string;
   };
+  Constructors: Array<{
+    constructorId: string;
+    url: string;
+    name: string;
+    nationality: string;
+  }>;
+};
 
 interface Race {
-	season: string;
-	raceName: string;
-	Results: {
-		Constructor: {
-			name: string;
-			nationality: string;
-		};
-	}[];
+  season: string;
+  raceName: string;
+  Results: {
+    Constructor: {
+      name: string;
+      nationality: string;
+    };
+  }[];
 }
 
 type ConstructorStanding = {
-	position: string;
-	positionText: string;
-	points: string;
-	wins: string;
-	Constructor: {
-	  constructorId: string;
-	  url: string;
-	  name: string;
-	  nationality: string;
-	};
+  position: string;
+  positionText: string;
+  points: string;
+  wins: string;
+  Constructor: {
+    constructorId: string;
+    url: string;
+    name: string;
+    nationality: string;
   };
-  
-  interface ConstructorState {
-	season: string;
-	round: string;
-	ConstructorStandings: ConstructorStanding[];
-  }
+};
 
-interface State {
-	season: string;
-	list: Race[];
-	results: {
-		name: string;
-		nationality: string;
-	};
+interface ConstructorState {
+  season: string;
+  round: string;
+  ConstructorStandings: ConstructorStanding[];
 }
 
+interface State {
+  season: string;
+  list: Race[];
+  results: {
+    name: string;
+    nationality: string;
+  };
+}
 
-export default function Default(props: {
+export default function Default(props: {}) {
+  const baseUrl = "http://localhost:8080/f1-graphics/driverstandings/";
+  const baseUrlContructios = "http://localhost:8080/f1-graphics/constructorstandings/";
+  const [constructionLeader, setConstructionLeader] = useState<ConstructorStanding>({
+    position: "",
+    positionText: "",
+    points: "",
+    wins: "",
+    Constructor: {
+      constructorId: "",
+      url: "",
+      name: "",
+      nationality: ""
+    }
+  });
 
-}) {
-	const baseUrl = "http://localhost:8080/f1-graphics/driverstandings/";
-	const baseUrlContructios = "http://localhost:8080/f1-graphics/constructorstandings/";
-	const [constructionLeader, setConstructionLeader] = useState<ConstructorStanding>({
-		position: "",
-		positionText: "",
-		points: "",
-		wins: "",
-		Constructor: {
-			constructorId: "",
-			url: "",
-			name: "",
-			nationality: ""
-		  }
-	  });
+  const [listConstruction, setListConstruction] = useState<ConstructorState>({
+    season: "",
+    round: "",
+    ConstructorStandings: [{
+      position: "",
+      positionText: "",
+      points: "",
+      wins: "",
+      Constructor: {
+        constructorId: "",
+        url: "",
+        name: "",
+        nationality: ""
+      }
+    }]
+  });
 
-	  const [listConstruction, setListConstruction] = useState<ConstructorState>({
-		season: "",
-		round: "",
-		ConstructorStandings: [{
-			position: "",
-			positionText: "",
-			points: "",
-			wins: "",
-			Constructor: {
-			constructorId: "",
-			url: "",
-			name: "",
-			nationality: ""
-		  }}
-		]
-	  });
-	  
-
-	  const [constructorStandingsList, setConstructorStandingsList] = useState<ConstructorStanding[]>([
-		{
-		  position: "",
-		  positionText: "",
-		  points: "",
-		  wins: "",
-		  Constructor: {
-			constructorId: "",
-			url: "",
-			name: "",
-			nationality: ""
-		  }
-		}
-	  ]);
-
-
-
+  const [constructorStandingsList, setConstructorStandingsList] = useState<ConstructorStanding[]>([
+    {
+      position: "",
+      positionText: "",
+      points: "",
+      wins: "",
+      Constructor: {
+        constructorId: "",
+        url: "",
+        name: "",
+        nationality: ""
+      }
+    }
+  ]);
 
   const [driverLeader, setDriverLeader] = useState({
     position: "",
@@ -142,38 +136,58 @@ export default function Default(props: {
     Constructors: [],
   });
 
+  const [listDriverStanding, setDriverStanding] = useState<DriverStanding[]>([
+	{position: "",
+    positionText: "",
+    points: "",
+    wins: "",
+    Driver: {
+      url: "",
+      code: "",
+      permanentNumber: "",
+      givenName: "",
+      familyName: "",
+      dateOfBirth: "",
+      nationality: "",
+      driverId: "",
+    },
+    Constructors: [],}
+  ]);
+
+
   useEffect(() => {
     axios(baseUrl).then((resp) => {
-      	const drivers = resp.data[0].DriverStandings[0];
-    	setDriverLeader(drivers);
-    });
+		const driverStandings = resp.data[0].DriverStandings;
+		setDriverStanding(driverStandings);
+		const drivers = driverStandings[0];
+		setDriverLeader(drivers);
+	  });
 
     axios(baseUrlContructios).then((resp) => {
-		const listConstructors = resp.data[0].ConstructorStandings;
-    	const constructors = resp.data[0].ConstructorStandings[0];
-		setConstructorStandingsList(listConstructors)
-    	setConstructionLeader(constructors);
-		setListConstruction(listConstructors)
+      const listConstructors = resp.data[0].ConstructorStandings;
+      const constructors = resp.data[0].ConstructorStandings[0];
+      setConstructorStandingsList(listConstructors);
+      setConstructionLeader(constructors);
+      setListConstruction(listConstructors);
     });
   }, []);
 
-
   
 
-	return (
-		<Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
-			<SimpleGrid columns={{ base: 1, md: 2, lg: 3, '2xl': 6 }} gap='20px' mb='0px'>
-				<MiniStatisticsGrafic
-					endContent={
-						<Flex me='-16px' mt='10px'>
-							<FormLabel htmlFor='balance'>
-								<Avatar src={MAX} />
-							</FormLabel>
-						</Flex>
-					}
-					title="Driver Leader Points"
-					name={driverLeader.Driver.givenName +" " + driverLeader.Driver.familyName}
-					value={driverLeader.points}  />
+  return (
+    <Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3, '2xl': 6 }} gap='20px' mb='0px'>
+        <MiniStatisticsGrafic
+          endContent={
+            <Flex me='-16px' mt='10px'>
+              <FormLabel htmlFor='balance'>
+                <Avatar src={MAX} />
+              </FormLabel>
+            </Flex>
+          }
+          title="Driver Leader Points"
+          name={driverLeader.Driver.givenName +" " + driverLeader.Driver.familyName}
+          value={driverLeader.points}  />
         <MiniStatisticsGrafic
           endContent={
             <Flex me="-16px" mt="10px">
@@ -186,17 +200,13 @@ export default function Default(props: {
           name={`${constructionLeader.Constructor.name || 'N/A'}`}
           value={`${constructionLeader.points || 'N/A'}`}
         />
-		 <div style={{ display: 'flex' }}> 
+		<div style={{ display: 'flex' }}> 
     <ConstructorStandingsStatic constructorStandings={constructorStandingsList} />
   </div>
-  <div style={{ display: 'flex', marginLeft: '95px', width: 'calc(100% + 110px)'  }}> {/* Add margin-right for spacing */}
-    <ConstructorStandingsStatic constructorStandings={constructorStandingsList} />
+  <div style={{ display: 'flex', marginLeft: '95px', width: 'calc(100% + 10px)'  }}> {/* Add margin-right for spacing */}
+    <DriverStandingsStatic driverStandings={listDriverStanding} />
   </div>
-    
-    
       </SimpleGrid>
-
-      
-		</Box>
-	);
+    </Box>
+  );
 }
