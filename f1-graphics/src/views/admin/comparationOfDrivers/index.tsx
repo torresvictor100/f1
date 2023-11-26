75d25ba0358abf090b9dd61da991b9a9197182d4
torresvictor100 <torresvictor100@gmail.com>
2023-11-26 13:46:46 -0300

    iniciando

--- f1-graphics/src/views/admin/comparationOfDrivers/index.tsx
+++ f1-graphics/src/views/admin/comparationOfDrivers/index.tsx
@@ -23,30 +23,15 @@
 import React, { forwardRef } from 'react';
 
 // Chakra imports
-import { Avatar, Box,StatLabel, Flex, FormLabel,Text, Icon, Select, SimpleGrid,useStyleConfig, useColorModeValue } from '@chakra-ui/react';
-// Assets
-import Mclaren from 'assets/img/comparationOfDrivers/Mclaren.png';
-import RBR from 'assets/img/comparationOfDrivers/Red-Bull-logo.jpg';
-import MaxVerstappen from 'assets/img/comparationOfDrivers/max-verstappen.png';
-import Hamiton from 'assets/img/comparationOfDrivers/Hamiton.jpg';
+import { Box,useStyleConfig, useColorModeValue } from '@chakra-ui/react';
 // Custom components
-import MiniCalendar from 'components/calendar/MiniCalendar';
-import MiniStatisticsTitle from 'components/card/MiniStatisticsTitle'
-import CheckTable from 'views/admin/rtl/components/CheckTable';
-import ComplexTable from 'views/admin/default/components/ComplexTable';
-import DailyTraffic from 'views/admin/default/components/DailyTraffic';
-import PieCard from 'views/admin/default/components/PieCard';
-import Tasks from 'views/admin/default/components/Tasks';
 import TotalSpent from 'views/admin/default/components/TotalSpent';
-import WeeklyRevenue from 'views/admin/default/components/WeeklyRevenue';
-import tableDataCheck from 'views/admin/default/variables/tableDataCheck';
-import tableDataComplex from 'views/admin/default/variables/tableDataComplex';
 import { CustomCardProps } from 'theme/theme';
+import StatisticsTable from './../home/components/StatisticsTable'
 
 export default function UserReports() {
 	// Chakra Color Mode
-	const brandColor = useColorModeValue('brand.500', 'white');
-	const boxBg = useColorModeValue('secondaryGray.300', 'whiteAlpha.100');
+
 
 	const CustomCard = forwardRef<HTMLDivElement, CustomCardProps>((props, ref) => {
 		const { size, variant, ...rest } = props;
@@ -59,72 +44,8 @@ export default function UserReports() {
 	
 	return (
 
-		<Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
-
-		
-		<CustomCard py='35px'  mb='10'>
-        {/* Use Chakra UI text utilities or add custom styles here */}
-        <Text fontSize="2xl" fontWeight="bold" textAlign="left" p="-0.5"  color={textColor2023} >
-			Season 2023
-        </Text>
-      </CustomCard>
-			<SimpleGrid columns={{ base: 1, md: 2, lg: 3, '2xl': 6 }} gap='20px' mb='20px'>
-			<MiniStatisticsTitle
-					endContent={
-						<Flex me='-16px' mt='10px'>
-							<FormLabel htmlFor='balance'>
-								<Avatar src={RBR} />
-							</FormLabel>
-						</Flex>
-					}
-					title="Season Leader Construction"
-					name='Red Bull'
-					value='822'
-				/>
-				<MiniStatisticsTitle
-					endContent={
-						<Flex me='-16px' mt='10px'>
-							<FormLabel htmlFor='balance'>
-								<Avatar src={MaxVerstappen} />
-							</FormLabel>
-						</Flex>
-					}
-					title="Season Leader Driver"
-					name='M. Verstappen'
-					value='549'
-				/>
-				<MiniStatisticsTitle
-					endContent={
-						<Flex me='-16px' mt='10px'>
-							<FormLabel htmlFor='balance'>
-								<Avatar src={Mclaren} />
-							</FormLabel>
-							<Select id='balance' variant='mini' mt='5px' me='0px' defaultValue='usd'>
-								<option value='mclaren'>Mclaren</option>
-								<option value='ferrari'>Ferrari</option>
-								<option value='mercedes'>Mercedes</option>
-							</Select>
-						</Flex>
-					}
-					title="Constructor Points"
-					name='McLaren'
-					value='284'
-				/>
-				<MiniStatisticsTitle
-					endContent={
-						<Flex me='-16px' mt='10px'>
-							<FormLabel htmlFor='balance'>
-								<Avatar src={Hamiton} />
-							</FormLabel>
-							<Select id='balance' variant='mini' mt='5px' me='0px' defaultValue='usd'>
-							</Select>
-						</Flex>
-					}
-					title="Driver Points"
-					name='L. Hamiton'
-					value='232'
-				/>
-			</SimpleGrid>
+		<Box>		
+			<StatisticsTable/>
 			<TotalSpent />
 			
 		</Box>
--- f1-graphics/src/views/admin/default/components/TotalSpent.tsx
+++ f1-graphics/src/views/admin/default/components/TotalSpent.tsx
@@ -1,18 +1,99 @@
 // Chakra imports
-import { Avatar, Box,StatLabel, Flex, FormLabel,Text, Icon, Select, SimpleGrid,useStyleConfig, useColorModeValue } from '@chakra-ui/react';
+import { Avatar, Box,StatLabel, Flex, FormLabel,Text, Select, SimpleGrid,useStyleConfig, useColorModeValue } from '@chakra-ui/react';
 // Custom components
 import Card from 'components/card/Card';
 import LineChart from 'components/charts/LineChart';
-import { IoCheckmarkCircle } from 'react-icons/io5';
 import { MdBarChart, MdOutlineCalendarToday } from 'react-icons/md';
 // Assets
-import { RiArrowUpSFill } from 'react-icons/ri';
+
 import { lineChartDataTotalSpent, lineChartOptionsTotalSpent } from 'variables/charts';
 import Hamiton from 'assets/img/comparationOfDrivers/Hamiton.jpg';
 import Massa from 'assets/img/comparationOfDrivers/Massa.jpg';
 import Soma from 'assets/img/comparationOfDrivers/Soma.jpg';
 import MiniStatisticsTitle from 'components/card/MiniStatisticsTitle'
 import MiniStatistics from 'components/card/MiniStatistics'
+
+
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+interface Location {
+	lat: string;
+	locality: string;
+	country: string;
+  }
+  
+  interface Circuit {
+	circuitId: string;
+	url: string;
+	circuitName: string;
+	Location: Location;
+  }
+  
+  interface Time {
+	time: string;
+  }
+  
+  
+  interface AverageSpeed {
+	units: string;
+	speed: string;
+  }
+  
+  interface FastestLap {
+	rank: string;
+	lap: string;
+	Time: Time;
+	AverageSpeed: AverageSpeed;
+  }
+  
+  interface Constructor {
+	constructorId: string;
+	url: string;
+	name: string;
+	nationality: string;
+  }
+  
+  interface ResultSeaseon {
+	raceName: string;
+	pointsSeason: number;
+	pointsSeasonRaces: number;
+	pointsSeasonSprintRaces: number;
+  }
+  
+  interface ResultRace {
+	position: string;
+	points: string;
+	Time: Time;
+	FastestLap: FastestLap;
+  }
+  
+  interface DriversResults {
+	driverId: string;
+	url: string;
+	givenName: string;
+	dateOfBirth: string;
+	nationality: string;
+	Constructor: Constructor;
+	ResultSeaseon: ResultSeaseon;
+	ResultRace: ResultRace;
+  }
+  
+  interface RaceSeason {
+	round: string;
+	url: string;
+	raceName: string;
+	date: string;
+	time: string;
+	Circuit: Circuit;
+	DriversResults: DriversResults[];
+  }
+  
+  interface SeasonInfo {
+	season: string;
+	totalGPs: string;
+	raceSeason: RaceSeason[];
+  }
 
 export default function TotalSpent(props: { [x: string]: any }) {
 	const { ...rest } = props;
@@ -26,6 +107,85 @@ export default function TotalSpent(props: { [x: string]: any }) {
 	const bgButton = useColorModeValue('secondaryGray.300', 'whiteAlpha.100');
 	const bgHover = useColorModeValue({ bg: 'secondaryGray.400' }, { bg: 'whiteAlpha.50' });
 	const bgFocus = useColorModeValue({ bg: 'secondaryGray.300' }, { bg: 'whiteAlpha.100' });
+
+	const baseUrl = "http://localhost:8080/f1-graphics/seasons/season-drivers-ids/2021?listDriversIdRequestDTO=max_verstappen";
+	const [dataLoaded, setDataLoaded] = useState(false);
+	const [season, setSeason] = useState<SeasonInfo>(
+		{
+		season: "",
+  	totalGPs: "",
+  	raceSeason: [
+    {
+      round: "",
+      url: "",
+      raceName: "",
+      date: "",
+      time: "",
+      Circuit: {
+        circuitId: "",
+        url: "",
+        circuitName: "",
+        Location: {
+          lat: "",
+          locality: "",
+          country: "",
+        },
+      },
+      DriversResults: [
+        {
+          driverId: "",
+          url: "",
+          givenName: "",
+          dateOfBirth: "",
+          nationality: "",
+          Constructor: {
+            constructorId: "",
+            url: "",
+            name: "",
+            nationality: "",
+          },
+          ResultSeaseon: {
+            raceName: "",
+            pointsSeason: 0,
+            pointsSeasonRaces: 0,
+            pointsSeasonSprintRaces: 0,
+          },
+          ResultRace: {
+            position: "",
+            points: "",
+            Time: {
+              time: "",
+            },
+            FastestLap: {
+              rank: "",
+              lap: "",
+              Time: {
+                time: "",
+              },
+              AverageSpeed: {
+                units: "",
+                speed: "",
+              },
+            },
+          },
+        },
+      ],
+    },
+  ],
+		}
+	);
+
+
+	useEffect(() => {
+		axios(baseUrl)
+		  .then((resp) => {
+			const season2 = resp.data;
+			setSeason(season2);
+		  })
+		  .finally(() => setDataLoaded(true)); // Define o estado para true após a conclusão da requisição
+	  }, []);
+
+	  console.log(season)
 	return (
 		<Card justifyContent='center' alignItems='center' flexDirection='column' w='100%' mb='0px' {...rest}>
 			<Text color={textColor} fontSize='54px' textAlign='start' fontWeight='700' lineHeight='100%'>
