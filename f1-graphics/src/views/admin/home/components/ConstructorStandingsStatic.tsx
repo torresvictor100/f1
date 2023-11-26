43f62401aeefc6802c2ed3f4a8b2b5ef32e70c4a
João Paulo <jpcchaves@outlook.com>
2023-11-26 15:45:29 -0300

    fix: adjusting statistics card

--- f1-graphics/src/views/admin/home/components/ConstructorStandingsStatic.tsx
+++ f1-graphics/src/views/admin/home/components/ConstructorStandingsStatic.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { Flex, FormLabel, Avatar, Select, Box } from '@chakra-ui/react';
-import MiniStatisticsTitle from 'components/card/MiniStatisticsTitle';
-import McLaren from 'assets/img/comparationOfDrivers/Mclaren.png';
+import { Avatar, Flex, FormLabel, Select } from "@chakra-ui/react";
+import McLaren from "assets/img/comparationOfDrivers/Mclaren.png";
+import MiniStatisticsTitle from "components/card/MiniStatisticsTitle";
+import React, { useState } from "react";
 
 interface ConstructorStanding {
   position: string;
@@ -20,44 +20,58 @@ interface ConstructorSelectProps {
   constructorStandings: ConstructorStanding[];
 }
 
-const ConstructorStandingsStatic: React.FC<ConstructorSelectProps> = ({ constructorStandings }) => {
-  const [selectedConstructor, setSelectedConstructor] = useState<ConstructorStanding>(constructorStandings[0]);
+const ConstructorStandingsStatic: React.FC<ConstructorSelectProps> = ({
+  constructorStandings,
+}) => {
+  const [selectedConstructor, setSelectedConstructor] =
+    useState<ConstructorStanding>(constructorStandings[0]);
 
-  const handleConstructorChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleConstructorChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     const selectedConstructorId = event.target.value;
-    const selectedConstructor = constructorStandings.find(constructor => constructor.Constructor.constructorId === selectedConstructorId);
+    const selectedConstructor = constructorStandings.find(
+      (constructor) =>
+        constructor.Constructor.constructorId === selectedConstructorId
+    );
     setSelectedConstructor(selectedConstructor || constructorStandings[0]);
   };
 
   return (
-    <Box width="calc(100% - -100px)">
-      <MiniStatisticsTitle
-        endContent={
-          <Flex me="-16px" mt="10px">
-            <FormLabel htmlFor="balance">
-              <Avatar src={McLaren} />
-            </FormLabel>
-            <Select
-              id="balance"
-              mt="5px"
-              me="0px"
-              defaultValue={selectedConstructor.Constructor.constructorId}
-              onChange={handleConstructorChange}
+    // <Box width="calc(100% - -100px)">
+    <MiniStatisticsTitle
+      endContent={
+        <Flex mt="10px">
+          <FormLabel htmlFor="balance">
+            <Avatar src={McLaren} />
+          </FormLabel>
+        </Flex>
+      }
+      hasSelect
+      selectElement={
+        <Select
+          id="balance"
+          mt="5px"
+          me="0px"
+          defaultValue={selectedConstructor.Constructor.constructorId}
+          onChange={handleConstructorChange}
+        >
+          <option value="">Constructor</option>
+          {constructorStandings.map((constructor) => (
+            <option
+              key={constructor.Constructor.constructorId}
+              value={constructor.Constructor.constructorId}
             >
-              <option value="">Constructor</option>
-              {constructorStandings.map(constructor => (
-                <option key={constructor.Constructor.constructorId} value={constructor.Constructor.constructorId}>
-                  {constructor.Constructor.name}
-                </option>
-              ))}
-            </Select>
-          </Flex>
-        }
-        title="Points"
-        name={selectedConstructor.Constructor.name}
-        value={selectedConstructor.points}
-      />
-    </Box>
+              {constructor.Constructor.name}
+            </option>
+          ))}
+        </Select>
+      }
+      title="Points"
+      name={selectedConstructor.Constructor.name}
+      value={selectedConstructor.points}
+    />
+    // </Box>
   );
 };
 
--- f1-graphics/src/views/admin/home/components/DriverStandingsStatic.tsx
+++ f1-graphics/src/views/admin/home/components/DriverStandingsStatic.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { Flex, FormLabel, Avatar, Select, Box } from '@chakra-ui/react';
-import MiniStatisticsTitle from 'components/card/MiniStatisticsTitle';
-import PlaceholderImage from 'assets/img/comparationOfDrivers/Mclaren.png';
-import MAX from 'assets/img/DriversImage/ricciardo.jpg';
+import React, { useState } from "react";
+import { Flex, FormLabel, Avatar, Select, Box } from "@chakra-ui/react";
+import MiniStatisticsTitle from "components/card/MiniStatisticsTitle";
+import PlaceholderImage from "assets/img/comparationOfDrivers/Mclaren.png";
+import MAX from "assets/img/DriversImage/ricciardo.jpg";
 
 interface DriverStanding {
   position: string;
@@ -31,44 +31,45 @@ interface DriverSelectProps {
   driverStandings: DriverStanding[];
 }
 
-const DriverStandingsStatic: React.FC<DriverSelectProps> = ({ driverStandings }) => {
-  const [selectedDriver, setSelectedDriver] = useState<DriverStanding>(driverStandings[0]);
+const DriverStandingsStatic: React.FC<DriverSelectProps> = ({
+  driverStandings,
+}) => {
+  const [selectedDriver, setSelectedDriver] = useState<DriverStanding>(
+    driverStandings[0]
+  );
 
   const handleDriverChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedDriverId = event.target.value;
-    const selectedDriver = driverStandings.find(driver => driver.Driver.driverId === selectedDriverId);
+    const selectedDriver = driverStandings.find(
+      (driver) => driver.Driver.driverId === selectedDriverId
+    );
     setSelectedDriver(selectedDriver || driverStandings[0]);
   };
 
   return (
-    <Box width="calc(100% - -100px)">
-      <MiniStatisticsTitle
-        endContent={
-          <Flex me="-16px" mt="10px">
-            <FormLabel htmlFor="balance">
-              <Avatar src={MAX} />
-            </FormLabel>
-            <Select
-              id="balance"
-              mt="5px"
-              me="0px"
-
-              onChange={handleDriverChange}
-            >
-               <option value="">Driver</option>
-              {driverStandings.map(driver => (
-                <option key={driver.Driver.driverId} value={driver.Driver.driverId}>
-                  {driver.Driver.familyName}
-                </option>
-              ))}
-            </Select>
-          </Flex>
-        }
-        title="Points"
-        name={`${selectedDriver.Driver.familyName}`}
-        value={selectedDriver.points}
-      />
-    </Box>
+    <MiniStatisticsTitle
+      endContent={
+        <Flex me="-16px" mt="10px">
+          <FormLabel htmlFor="balance">
+            <Avatar src={MAX} />
+          </FormLabel>
+        </Flex>
+      }
+      hasSelect
+      selectElement={
+        <Select id="balance" mt="5px" me="0px" onChange={handleDriverChange}>
+          <option value="">Driver</option>
+          {driverStandings.map((driver) => (
+            <option key={driver.Driver.driverId} value={driver.Driver.driverId}>
+              {driver.Driver.familyName}
+            </option>
+          ))}
+        </Select>
+      }
+      title="Points"
+      name={`${selectedDriver.Driver.familyName}`}
+      value={selectedDriver.points}
+    />
   );
 };
 
