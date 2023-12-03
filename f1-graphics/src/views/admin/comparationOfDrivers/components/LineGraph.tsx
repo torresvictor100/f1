30aaa275c4a532fb656f56e948780f042803c3c9
torresvictor100 <torresvictor100@gmail.com>
2023-12-03 08:01:24 -0300

    refatoração

--- f1-graphics/src/views/admin/comparationOfDrivers/components/LineGraph.tsx
+++ f1-graphics/src/views/admin/comparationOfDrivers/components/LineGraph.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import {
     Box
-  } from "@chakra-ui/react";
+} from "@chakra-ui/react";
 import LineChart from 'components/charts/LineChart';
 
 export default function Default(props: {
     chartData: any[];
     chartOptions: any;
-  }){
+}) {
     return (
         <Box minH='400px' minW='95%' mt='auto'>
 
-			&& <LineChart chartData={props.chartData} chartOptions={props.chartOptions} />
-		</Box>
+            <LineChart chartData={props.chartData} chartOptions={props.chartOptions} />
+            
+        </Box>
     );
 }
