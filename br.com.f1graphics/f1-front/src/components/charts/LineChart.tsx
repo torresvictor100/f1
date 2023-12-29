1b2ed094069ef34128fa96ee4cf421a4b5d9534f
torresvictor100 <torresvictor100@gmail.com>
2023-12-29 20:50:22 -0300

    add troca tamanhi

--- br.com.f1graphics/f1-front/src/components/charts/LineChart.tsx
+++ br.com.f1graphics/f1-front/src/components/charts/LineChart.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import ReactApexChart from 'react-apexcharts';
 
+import {
+	Box,
+	Slider,
+	SliderTrack,
+	SliderFilledTrack,
+	SliderThumb
+} from "@chakra-ui/react";
+
 type ChartProps = {
-	// using `interface` is also ok
-	[x: string]: any;
-};
-type ChartState = {
 	chartData: any[];
 	chartOptions: any;
 };
 
+type ChartState = {
+	chartData: any[];
+	chartOptions: any;
+	chartWidth: string;
+	chartHeight: string;
+};
+
 class LineChart extends React.Component<ChartProps, ChartState> {
-	constructor(props: { chartData: any[]; chartOptions: any }) {
+	constructor(props: ChartProps) {
 		super(props);
 
 		this.state = {
 			chartData: [],
-			chartOptions: {}
+			chartOptions: {},
+			chartWidth: '100%',
+			chartHeight: '500px',
 		};
 	}
-
 
 	componentDidMount() {
 		this.setState({
@@ -29,14 +41,29 @@ class LineChart extends React.Component<ChartProps, ChartState> {
 	}
 
 	render() {
+		const { chartWidth, chartHeight, chartData, chartOptions } = this.state;
+
 		return (
-			<ReactApexChart
-				options={this.state.chartOptions}
-				series={this.state.chartData}
-				type='line'
-				width='100%'
-				height='100%'
-			/>
+			<Box>
+				<ReactApexChart
+					width={chartWidth}
+					height={chartHeight}
+					options={chartOptions}
+					series={chartData}
+					type='line'
+				/>
+				<Slider
+					min={0}
+					max={100}
+					value={parseInt(chartWidth)}
+					onChange={(value) => this.setState({ chartWidth: `${value}%` })}
+				>
+					<SliderTrack bg="#f4f7fe">
+						<SliderFilledTrack bg="#1B2559" />
+					</SliderTrack>
+					<SliderThumb boxSize={6} borderColor="#1B2559" />
+				</Slider>
+			</Box>
 		);
 	}
 }
