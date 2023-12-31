50eae5c1cf0671727d6c375b01cfc546bfcf0a0c
torresvictor100 <torresvictor100@gmail.com>
2023-12-31 15:06:21 -0300

    refection

--- br.com.f1graphics/f1-front/src/views/admin/comparationOfDrivers/components/ChartOptions.tsx
+++ br.com.f1graphics/f1-front/src/views/admin/comparationOfDrivers/components/ChartOptions.tsx
@@ -1,45 +1,61 @@
 export const f1GraphicsChartOptions: any = {
+    //Title
     Title: 'F1Graphics.com.br',
     TitleColor: '#1B2559',
-    YaxisTitle: 'Points',
-    LabelColor: '#EE0000',
-    BackgroundColor: '#F4F7FE',
-    YaxisColor: '#1B2559',
-    LinesColor: '#1B2559',
-    LegendLabelsColor: '#EE0000',
-    YaxisLabelColor: '#EE0000',
     TitleFontSize: '28',
-    LabelFontSize: '13',
-    LegendLabelFontSize: '14',
+    //Background
+    BackgroundColor: '#F4F7FE',
+    //Yaxis
+    YaxisShow: true,
+    YaxisTitle: 'Points',
     YaxisTitleFontSize: '20',
+    YaxisColor: '#1B2559',
     YaxisLabelFontSize: '16',
-    ChartYaxisShow: true,
+    YaxisLabelColor: '#EE0000',
+    //Lines
+    YaxisLinesShow: true,
+    XaxisLinesShow: true,
+    LinesColor: '#1B2559',
+    //Label
+    LabelColor: '#EE0000',
+    LabelFontSize: '13',
+    //Legend
     ShowLegend: true,
-    ChartYaxisLinesShow: true,
+    LegendLabelsColor: '#EE0000',
+    LegendLabelFontSize: '14',
+    //Tooltip
     ChartTooltipShow: true,
     ChartTooltipTheme: 'light',
     chartTooltipShowInTop: false,
 };
 
 export const splashGoChartOptions: any = {
+    //Title
     Title: 'Splash/Go',
     TitleColor: '#019bd9',
-    YaxisTitle: 'Points',
-    LabelColor: '#FFFEFC',
-    BackgroundColor: '#00313F',
-    YaxisColor: '#019bd9',
-    LinesColor: '#FFFEFC',
-    LegendLabelsColor: '#FFFEFC',
-    YaxisLabelColor: '#FFFEFC',
     TitleFontSize: '28',
-    LabelFontSize: '13',
-    LegendLabelFontSize: '14',
+    //Background
+    BackgroundColor: '#00313F',
+    //Yaxis
+    YaxisShow: true,
+    YaxisTitle: 'Points',
     YaxisTitleFontSize: '20',
+    YaxisColor: '#019bd9',
     YaxisLabelFontSize: '16',
-    ChartYaxisShow: true,
+    YaxisLabelColor: '#FFFEFC',
+    //Lines
+    YaxisLinesShow: true,
+    XaxisLinesShow: true,
+    LinesColor: '#FFFEFC',
+    //Label
+    LabelColor: '#FFFEFC',
+    LabelFontSize: '13',
+    //Legend
     ShowLegend: true,
-    ChartYaxisLinesShow: true,
-    ChartTooltipShow: true,
-    ChartTooltipTheme: 'dark',
-    chartTooltipShowInTop: false,
+    LegendLabelsColor: '#FFFEFC',
+    LegendLabelFontSize: '14',
+    //Tooltip
+    TooltipShow: true,
+    TooltipTheme: 'dark',
+    TooltipShowInTop: false,
 };
